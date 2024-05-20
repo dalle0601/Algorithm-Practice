e4ac7f2a9522687754c27e7bb6837b73813b6693
KimHwaYoung <dalle0601@naver.com>
2024-05-20 22:12:05 +0900

    [level 2] Title: 전화번호 목록, Time: 248.06 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -21,7 +21,7 @@ class Solution {
         }
         
         for (String phone : phone_book) {
-            for (int i = 1; i < phone.length(); i++) {
+            for (int i = 0; i < phone.length(); i++) {
                 if (set.contains(phone.substring(0, i))) {
                     return false;
                 }
